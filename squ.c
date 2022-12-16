a3183cc86e7ade80f09324c5965fcbf84cc3efff
Thatagricguy <samueljohn3999@gmail.com>
2022-12-16 22:10:15 +0100

    square root of a number

---
+++ squ.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+// defines a function SQR(a) returning (a * a) and with 'a' being a parameter
+#define SQR(a) (a * a)
+
+int main() {
+	int num = 3;
+	
+	printf("Enter a number to find its square : ");
+	
+	
+	
+	
+	printf("Square of %d : %d", num, SQR(num));
+	
+	return 0;
+}
